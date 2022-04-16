paranoidandroid2124/CondensedChess

commit 0d59a60d2fe954b48163cd9d0760b6e4ce79c899

Support pgn escape mechanism % (#284)
* Support pgn escape mechanism %

* Rename whitespace to escape

* fmt

src/main/scala/format/pgn/Parser.scala
@@ -11,19 +11,12 @@ import cats.data.NonEmptyList
 // http://www.saremba.de/chessgml/standards/pgn/pgn-complete.htm
 object Parser {
 
-  val whitespace  = R.lf | R.wsp
-  val whitespaces = whitespace.rep0.?
+  val whitespace = R.lf | R.wsp
+  val pgnComment = P.caret.filter(_.col == 0) *> P.char('%') *> P.until(P.char('\n')).void
+  // pgnComment with % or whitespaces
+  val escape = pgnComment.? *> whitespace.rep0.?
 
-  def full(pgn: String): Validated[String, ParsedPgn] = {
-    val preprocessed = augmentString(pgn).linesIterator
-      .filterNot {
-        _.headOption.contains('%')
-      }
-      .mkString("\n")
-    parse(preprocessed)
-  }
-
-  private def parse(pgn: String): Validated[String, ParsedPgn] =
+  def full(pgn: String): Validated[String, ParsedPgn] =
     pgnParser.parse(pgn) match {
       case Right((_, parsedResult)) =>
         valid(parsedResult)
@@ -63,7 +56,7 @@ object Parser {
 
     val inlineCommentary: P[String] = P.char(';') *> P.until(R.lf)
 
-    val commentary = (blockCommentary | inlineCommentary).withContext("Invalid comment") <* whitespaces
+    val commentary = (blockCommentary | inlineCommentary).withContext("Invalid comment") <* escape
     val resultList = List(
       "*",
       "1/2-1/2",
@@ -103,7 +96,7 @@ object Parser {
 
     val nag = (P.char('$') ~ R.digit.rep).string | nagGlyphsRE
 
-    val nagGlyphs: P0[Glyphs] = (nag <* whitespaces).rep0.map(nags =>
+    val nagGlyphs: P0[Glyphs] = (nag <* escape).rep0.map(nags =>
       Glyphs fromList nags.flatMap {
         Glyph.find
       }
@@ -128,18 +121,18 @@ object Parser {
     val strMove: P[San] = P
       .recursive[San] { recuse =>
         val variation: P[Sans] =
-          (((P.char('(') <* whitespaces) *> recuse.rep0 <* (P.char(')') ~ whitespaces)) <* whitespaces)
+          (((P.char('(') <* escape) *> recuse.rep0 <* (P.char(')') ~ escape)) <* escape)
             .map(Sans(_))
 
-        ((number.backtrack | (commentary <* whitespaces)).rep0 ~ forbidNullMove).with1 *>
-          (((MoveParser.moveWithSuffix ~ nagGlyphs ~ commentary.rep0 ~ nagGlyphs ~ variation.rep0) <* moveExtras.rep0) <* whitespaces).backtrack
+        ((number.backtrack | (commentary <* escape)).rep0 ~ forbidNullMove).with1 *>
+          (((MoveParser.moveWithSuffix ~ nagGlyphs ~ commentary.rep0 ~ nagGlyphs ~ variation.rep0) <* moveExtras.rep0) <* escape).backtrack
             .map { case ((((san, glyphs), comments), glyphs2), variations) =>
               san withComments comments withVariations variations mergeGlyphs (glyphs merge glyphs2)
             }
       }
 
     val strMoves: P0[(InitialPosition, List[San], Option[String])] =
-      ((commentary.rep0 ~ strMove.rep0) ~ (result <* whitespaces).? <* commentary.rep0).map {
+      ((commentary.rep0 ~ strMove.rep0) ~ (result <* escape).? <* commentary.rep0).map {
         case ((coms, sans), res) => (InitialPosition(cleanComments(coms)), sans.toList, res)
       }
   }
@@ -224,7 +217,7 @@ object Parser {
     )
 
     val move: P[San] = (castle | standard).withContext("Invalid chess move")
-    val moveWithSuffix: P[San] = (move ~ suffixes <* whitespaces)
+    val moveWithSuffix: P[San] = (move ~ suffixes <* escape)
       .map { case (std, suf) =>
         std withSuffixes suf
       }
@@ -254,7 +247,7 @@ object Parser {
   }
 
   private val tagsAndMovesParser: P0[ParsedPgn] =
-    ((whitespaces *> TagParser.tags.?) ~ MovesParser.strMoves.?).map {
+    ((escape *> TagParser.tags.? <* escape) ~ MovesParser.strMoves.?).map {
       case (optionalTags, optionalMoves) => {
         val preTags = Tags(optionalTags.map(_.toList).getOrElse(List()))
         optionalMoves match {
@@ -269,7 +262,7 @@ object Parser {
     }
 
   private val pgnParser: P0[ParsedPgn] =
-    whitespaces *> P.string("[pgn]").? *> tagsAndMovesParser <* P.string("[/pgn]").? <* whitespaces
+    escape *> P.string("[pgn]").? *> tagsAndMovesParser <* P.string("[/pgn]").? <* escape
 
   private def showExpectations(context: String, str: String, error: P.Error): String = {
     val lm  = LocationMap(str)

src/test/scala/format/pgn/ParserTest.scala
@@ -10,6 +10,15 @@ class ParserTest extends ChessTest {
   val parser    = Parser.full _
   val parseMove = Parser.move _
 
+  "pgnComment" should {
+    "parse valid comment" in {
+      Parser.pgnComment.parse("% comment") must beRight
+    }
+    "parse invalid comment" in {
+      Parser.pgnComment.parse("  %comment") must beLeft
+    }
+  }
+
   "promotion check" should {
     "as a queen" in {
       parser("b8=Q ") must beValid.like { case a =>
@@ -344,6 +353,12 @@ class ParserTest extends ChessTest {
 
   "example with tags & comments without moves 2" in {
     parser(tagsCommentsWithoutMoves2) must beValid
+  }
+
+  "game with comments" in {
+    parser(gameWithComments) must beValid.like { case a =>
+      a.sans.value.size must_== 106
+    }
   }
 
 }

src/test/scala/format/pgn/Fixtures.scala
@@ -2027,6 +2027,37 @@ val tagsCommentsWithoutMoves2 ="""
 { Мат в 1 ход, ход белых }
 *
 """
+
+val gameWithComments = """[Event "The Match - Braingames World Chess Cham"]
+[Site "London"]
+[Date "2000.01.04"]
+[EventDate "2000.10.12"]
+[Round "3"]
+[Result "1/2-1/2"]
+[White "Garry Kasparov"]
+[Black "Vladimir Kramnik"]
+[ECO "C67"]
+[WhiteElo "2849"]
+[BlackElo "2770"]
+[PlyCount "106"]
+
+% this is a random comment
+1. e4 e5 2. Nf3 Nc6 3. Bb5 Nf6 4. O-O Nxe4
+
+% this is another comment
+5. d4 Nd6 6. Bxc6
+% this is another comment
+dxc6 7. dxe5 Nf5 8. Qxd8+ Kxd8 9. Nc3 Bd7 10. b3 h6 11. Bb2
+Kc8 12. Rad1 b6 13. Ne2 c5 14. c4 Bc6 15. Nf4 Kb7 16. Nd5 Ne7
+17. Rfe1 Rg8 18. Nf4 g5 19. Nh5 Rg6 20. Nf6 Bg7 21. Rd3 Bxf3
+22. Rxf3 Bxf6 23. exf6 Nc6 24. Rd3 Rf8 25. Re4 Kc8 26. f4 gxf4
+27. Rxf4 Re8 28. Bc3 Re2 29. Rf2 Re4 30. Rh3 a5 31. Rh5 a4
+32. bxa4 Rxc4 33. Bd2 Rxa4 34. Rxh6 Rg8 35. Rh7 Rxa2 36. Rxf7
+Ne5 37. Rg7 Rf8 38. h3 c4 39. Re7 Nd3 40. f7 Nxf2 41. Re8+ Kd7
+42. Rxf8 Ke7 43. Rc8 Kxf7 44. Rxc7+ Ke6 45. Be3 Nd1 46. Bxb6
+c3 47. h4 Ra6 48. Bd4 Ra4 49. Bxc3 Nxc3 50. Rxc3 Rxh4 51. Rf3
+Rh5 52. Kf2 Rg5 53. Rf8 Ke5 1/2-1/2"""
+
 val gamesForPerfTest = List(enpassantEP, enpassantEP2, recentChessCom, chessComCrazyhouse, fromPosProdCloseChess,
   fromChessProgrammingWiki, noTagButResult, inlineTags, whiteResignsInTags, whiteResignsInMoves, whiteResignsInTagsAndMoves,
   festivalFigueira, crazyhouseFromProd, complete960, fromWikipedia, stLouisFischerandom, inlineComments, fromChessgames,

commit f94de5b4cf44820c58fb280eaca9b897cda95853

carriage return \r breaks PGN parser
since #284 0d59a60d2fe954b48163cd9d0760b6e4ce79c899

caused by removal of `.linesIterator.mkString("\n")`

fixes lichess-org/lila#10815

src/main/scala/format/pgn/Parser.scala
@@ -17,7 +17,7 @@ object Parser {
   val escape = pgnComment.? *> whitespace.rep0.?
 
   def full(pgn: String): Validated[String, ParsedPgn] =
-    pgnParser.parse(pgn) match {
+    pgnParser.parse(pgn.replace("\r", "")) match {
       case Right((_, parsedResult)) =>
         valid(parsedResult)
       case Left(err) =>

src/test/scala/format/pgn/ParserTest.scala
@@ -36,6 +36,24 @@ class ParserTest extends ChessTest {
     }
   }
 
+  "carriage return" in {
+    "none" in {
+      parser("1. e4 c6\n2. d4 d5") must beValid.like { case parsed =>
+        parsed.sans.value.size must_== 4
+      }
+    }
+    "one" in {
+      parser("1. e4 c6\r\n2. d4 d5") must beValid.like { case parsed =>
+        parsed.sans.value.size must_== 4
+      }
+    }
+    "two" in {
+      parser("1. e4 c6\r\r\n2. d4 d5") must beValid.like { case parsed =>
+        parsed.sans.value.size must_== 4
+      }
+    }
+  }
+
   "result" in {
     "no tag but inline result" in {
       parser(noTagButResult) must beValid.like { case parsed =>